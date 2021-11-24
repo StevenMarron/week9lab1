ff1be4bfac50e1dc301c5a1ec23fa2f32b7b3104
StevenMarron <stevomarron@hotmail.com>
2021-11-24 18:23:25 +0000

    week9lab1

---
+++ script.js
@@ -0,0 +1,18 @@
+// Week 9 Lab 99Bottles SCRIPT
+function lyrics(){
+	for(i=99;i>=0;i--){
+			if(i>=3){
+				 console.log(i+" bottles of beer on the wall, " +i+ " bottles of beer, Take one down and pass it around "+(i-1)+ " bottles of beer on the wall!");
+
+        	}
+        	else if(i==2){
+				 console.log(i+" bottles of beer on the wall, " +i+ " bottles of beer, Take one down and pass it around "+(i-1)+ " bottle of beer on the wall!");
+        	}
+        	else if(i==1){
+				 console.log(i+" bottle of beer on the wall, " +i+ " bottle of beer, Take one down and pass it around, no more bottles of beer on the wall!");
+        	}
+        	else{
+				 console.log("No more bottles of beer on the wall, no more bottles of beer! Go to the store and buy some more, 99 bottles of beer on the wall!");
+			}
+		}
+}	
